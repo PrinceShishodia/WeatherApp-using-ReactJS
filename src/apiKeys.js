623148a2cf022d7198ec554a7b8bc77abc94b925
PrinceShishodia <95168097+PrinceShishodia@users.noreply.github.com>
2023-05-26 23:29:14 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "Enter the API Key",   //Just enter your API key in place of text, by accessing it from your account on https://api.openweathermap.org
+  base: "https://api.openweathermap.org/data/2.5/",
+};
